7c0b8c4c84fef49a4e93b34fce790b4d9a085ca7
pafnootiy <ksander-86@mail.ru>
2023-04-19 20:20:25 +0400

    add models

--- storage/stock_app/models.py
+++ storage/stock_app/models.py
@@ -1,3 +1,83 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Client(models.Model):
+    firstname = models.CharField('Имя', max_length=200)
+    lastname = models.CharField('Фамилия', max_length=200, null=True, blank=True)
+    phone = models.IntegerField('Номер телефона', null=True, blank=True)
+    email = models.EmailField('Почта')
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+    def __str__(self) -> str:
+        return f'{self.firstname} {self.lastname}'  # можно грохнуть и заменить User
+    
+
+
+
+class Tariff(models.Model):
+    title = models.CharField('Название тарифа', max_length=50)
+    price = models.IntegerField('Цена') 
+    days = models.IntegerField('Количество дней')
+
+    class Meta:
+        verbose_name = 'Тариф'
+
+     
+    def  __str__(self):
+        return self.title
+
+
+class Storage(models.Model):
+    title = models.CharField('Склад', max_length=100)
+    address = models.CharField('Адрес', max_length=200)
+
+    class Meta:
+        verbose_name = 'Склад'
+
+
+class Order(models.Model):
+    client = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name='clients',
+        verbose_name='Клиент'
+    )
+    address = models.CharField(
+        'Адрес',
+        max_length=200,
+        null=True,
+        blank=True
+    )
+    time = models.DateTimeField('Время создания заказа', auto_now=True)
+    comment = models.TextField(
+        'Комментарий',
+        max_length=200,
+        null=True,
+        blank=True
+    )
+    tariff = models.ForeignKey(
+        Tariff,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='tariffs',
+        verbose_name='Тариф'
+    )
+    qr_code = models.CharField(max_length=200, null=True, blank=False)
+    storage = models.ForeignKey(
+        Storage,
+        on_delete=models.CASCADE,
+        related_name='storages',
+        null=True,
+        verbose_name='Cклад'
+    )
+    paid_till = models.DateTimeField('Оплата до', null=True)
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def __str__(self) -> str:
+        return self

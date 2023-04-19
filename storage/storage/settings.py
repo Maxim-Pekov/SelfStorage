2c63d420ada0f851b4317b0769eefde653885b2c
Maxim-Pekov <max.pekov@gmail.com>
2023-04-19 22:51:33 +0300

    Add media root to settings.

--- storage/storage/settings.py
+++ storage/storage/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets'),
 ]

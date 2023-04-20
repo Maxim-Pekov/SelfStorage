9a252d546dd23f89ebd86fdbf2a62719bbeb9a26
Aleksy <aslepaugo@gmail.com>
2023-04-20 15:25:43 +0200

    Extend mail sending with inline images

--- storage/stock_app/views.py
+++ storage/stock_app/views.py
@@ -4,18 +4,8 @@ from mailapp.tasks import send_notification_mail
 
 
 def index(request):
-    print(request.method)
-    if request.method == 'POST':
-        user_mail = request.POST.get('EMAIL')
-        print(user_mail)
-        if user_mail:
-            send_notification_mail.delay(
-                subject='Welcome to our storage',
-                recipients=[user_mail],
-                message='Thank you for your attention to our storage service. We will reach you soon.'
-            )
-
-        return render(request, 'index.html')
+    if request.method == 'POST' and 'EMAIL' in request.POST:
+        process_welcome_email(request)
 
     return render(request, 'index.html')
 
@@ -38,3 +28,17 @@ def show_user_rent(request):
 
 def show_user_rent_empty(request):
     return render(request, 'my-rent-empty.html')
+
+
+def process_welcome_email(request):
+    user_mail = request.POST.get('EMAIL')
+    if user_mail:
+        send_notification_mail.delay(
+            subject='Welcome to our storage',
+            recipients=[user_mail],
+            template='welcome.html',
+            context={
+                'user_mail': user_mail,
+                'inline_images': ['img1.png'],
+            }
+        )
